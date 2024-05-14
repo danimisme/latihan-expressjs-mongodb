e96f95fded1c3dbf7f1eaaf0c20097cff423cd7a
danimisme <msubhanr53@gmail.com>
2024-05-14 17:04:32 +0700

    use express-ejs-layouts

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+
+app.use(expressLayout);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.set("layout", path.join(__dirname, "views/layouts"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", layout: "layouts/main-layout" });
 });
 
 app.listen(port, () => {
